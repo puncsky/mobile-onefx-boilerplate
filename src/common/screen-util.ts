c3383760783fe846a0ddcc02639df06b444fe6d0
yongmao.yu <yongmao.yu@qedge.cn>
2019-12-16 22:10:45 +0800

    feat: 调整安卓navigation  bar height

--- src/common/screen-util.ts
+++ src/common/screen-util.ts
@@ -1,11 +1,16 @@
 import { Dimensions, PixelRatio, Platform } from "react-native";
-
+import { StatusBar } from "react-native";
 const ScreenWidth = Dimensions.get("window").width;
 const ScreenHeight = Dimensions.get("window").height;
+const androidStatusBarHeight = StatusBar.currentHeight
+  ? StatusBar.currentHeight
+  : 0;
 const isIphoneX =
   Platform.OS === "ios" &&
   Number(`${ScreenHeight / ScreenWidth}`.substr(0, 4)) * 100 === 216;
-const statusBarHeight = Platform.OS === "ios" ? (isIphoneX ? 44 : 20) : 0;
-const navigationBarHeight = Platform.OS === "ios" ? (isIphoneX ? 88 : 64) : 59;
+const statusBarHeight =
+  Platform.OS === "ios" ? (isIphoneX ? 44 : 20) : androidStatusBarHeight;
+const navigationBarHeight =
+  Platform.OS === "ios" ? (isIphoneX ? 88 : 64) : 59 + androidStatusBarHeight;
 const onePx = 1 / PixelRatio.get();
 export { statusBarHeight, navigationBarHeight, onePx };
