89ccf61130d3048328b6d0c969f3e8e9c5d2ad8b
yuyongmao <yuyongmao@126.com>
2020-08-22 18:05:41 +0800

    fix:test error

--- src/common/__tests__/styled-text-test.tsx
+++ src/common/__tests__/styled-text-test.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as renderer from "react-test-renderer";
-import { MonoText } from "@/common/styled-text";
+import { MonoText } from "../styled-text";
 
 it("renders correctly", () => {
   const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
