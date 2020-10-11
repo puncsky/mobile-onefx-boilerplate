ff6057373aa0000446669337222b9bc7e14835ca
yuyongmao <yuyongmao@126.com>
2020-10-11 22:59:55 +0800

    feat:update push token and schema

--- src/common/register-push-token.tsx
+++ src/common/register-push-token.tsx
@@ -1,13 +1,6 @@
 import { Notifications } from "expo";
 import * as Permissions from "expo-permissions";
-import gql from "graphql-tag";
-import { apolloClient } from "@/common/apollo-client";
-
-export const ADD_PUSH_TOKEN = gql`
-  mutation addPushToken($pushToken: String) {
-    addPushToken(token: $pushToken)
-  }
-`;
+import { useAddPushTokenToRemote } from "@/common/push-token/use-push-token-to-remote";
 
 export const registerForPushNotificationAsync = async (): Promise<string> => {
   const { status: existingStatus } = await Permissions.getAsync(
@@ -22,17 +15,18 @@ export const registerForPushNotificationAsync = async (): Promise<string> => {
     return finalStatus;
   }
   const pushToken = await Notifications.getExpoPushTokenAsync();
+
   if (pushToken) {
     try {
-      await apolloClient.mutate({
-        mutation: ADD_PUSH_TOKEN,
-        variables: {
-          pushToken
-        }
-      });
-    } catch (error) {
+      const { mutate, error } = useAddPushTokenToRemote();
+      await mutate({ variables: { pushToken } });
+      if (error) {
+        // tslint:disable-next-line
+        console.log(`add push token fail ${error}`);
+      }
+    } catch (e) {
       // tslint:disable-next-line
-      console.log(`add push token fail ${error}`);
+      console.log(`add push token fail ${e}`);
     }
   }
   return finalStatus;
---
+++ src/screens/mine-screen/hooks/use-user-profile.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "react-apollo";
+import { userProfile } from "@/screens/mine-screen/data/query";
+import { UserProfile } from "@/screens/mine-screen/data/__generated__/UserProfile";
+
+export const useUserProfile = (userId: string) => {
+  const { data, error, loading } = useQuery<UserProfile>(userProfile, {
+    variables: { userId }
+  });
+  return {
+    email:
+      data && data.userProfile && data.userProfile.email
+        ? data.userProfile.email
+        : "",
+    error,
+    loading
+  };
+};
