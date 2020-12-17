e428dbe26e05efaac30aca2fd147d5848beefd66
yuyongmao <yuyongmao@126.com>
2020-12-18 00:14:29 +0800

    fix: delete unused files

--- src/common/styled-text.tsx
+++ src/common/styled-text.tsx
@@ -1,25 +1,16 @@
-// import * as React from "react";
-// import { StyleProp, Text, TextStyle } from "react-native";
-
-// interface Props {
-//   style?: StyleProp<TextStyle>;
-//   children: string;
-// }
-
-// export function MonoText(props: Props): JSX.Element {
-//   const { children } = props;
-//   return (
-//     <Text {...props} style={[props.style, { fontFamily: "space-mono" }]}>
-//       {children}
-//     </Text>
-//   );
-// }
 import * as React from "react";
+import { StyleProp, Text, TextStyle } from "react-native";
 
-import { Text, TextProps } from "./themed-text";
+interface Props {
+  style?: StyleProp<TextStyle>;
+  children: string;
+}
 
-export function MonoText(props: TextProps) {
+export function MonoText(props: Props): JSX.Element {
+  const { children } = props;
   return (
-    <Text {...props} style={[props.style, { fontFamily: "space-mono" }]} />
+    <Text {...props} style={[props.style, { fontFamily: "space-mono" }]}>
+      {children}
+    </Text>
   );
 }
