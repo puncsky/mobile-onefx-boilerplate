85a675a0bf9d858c6c94690a76dd032e6eae32a0
yuyongmao <yuyongmao@126.com>
2020-12-19 08:26:34 +0800

    fix: change image node version to 14

--- babel.config.js
+++ babel.config.js
@@ -3,14 +3,7 @@ const path = require("path");
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [
-      [
-        "babel-preset-expo",
-        {
-          runtime: "automatic"
-        }
-      ]
-    ],
+    presets: ["babel-preset-expo"],
     plugins: [
       [
         "module-resolver",
